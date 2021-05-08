54bd9a3558b40a6acb0f64c65dc7501d4e9bcf29
wenxuw <xu_wen@sfu.ca>
2021-05-08 15:17:21 -0700

    add app.py

--- project/__init__.py
+++ project/__init__.py
@@ -8,8 +8,8 @@ db = SQLAlchemy()
 
 
 def create_app():
-    app = Flask(__name__)
 
+    app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret-key-goes-here'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
